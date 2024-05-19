a78e5f38261d950ef8978be19dc4c13c212a5896
An0nimia <41164945+An0nimia@users.noreply.github.com>
2024-05-19 17:41:18 +0200

    Adding types to export

--- api_deezer/types/__init__.py
+++ api_deezer/types/__init__.py
@@ -1,7 +1,14 @@
-# pyright: reportUnusedImport=false
-
 from .track import Track
 from .album import Album
 from .chart import Chart
 from .artist import Artist
 from .playlist import Playlist
+
+
+__all__ = (
+	'Track',
+	'Album',
+	'Chart',
+	'Artist',
+	'Playlist'
+)
